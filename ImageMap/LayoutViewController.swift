acf01c91fe0dc4f2bfd64f40545b7ca94a1fd2c7
Engin KUK <kuk.engin@gmail.com>
2021-01-31 23:32:17 +0300

    added labels

---
+++ ImageMap/LayoutViewController.swift
@@ -0,0 +1,83 @@
+//
+//  LayoutViewController.swift
+//  ImageMap
+//
+//  Created by Engin KUK on 31.01.2021.
+//
+
+import UIKit
+
+class LayoutViewController: UIViewController, UIGestureRecognizerDelegate {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imageViewBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var imageViewLeadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var imageViewTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var imageViewTrailingConstraint: NSLayoutConstraint!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateMinZoomScaleForSize(view.bounds.size)
+    let doubleTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(scrollViewDoubleTapped))
+    doubleTapRecognizer.numberOfTapsRequired = 2
+    scrollView.addGestureRecognizer(doubleTapRecognizer)
+    }
+    
+    
+}
+ 
+extension LayoutViewController: UIScrollViewDelegate {
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        updateConstraintsForSize(view.bounds.size)
+    }
+  
+    @objc func scrollViewDoubleTapped(recognizer: UITapGestureRecognizer) {
+      
+        let pointInView = recognizer.location(in: imageView)
+ 
+      var newZoomScale = scrollView.zoomScale * 1.5
+      newZoomScale = min(newZoomScale, scrollView.maximumZoomScale)
+ 
+      let scrollViewSize = scrollView.bounds.size
+      let w = scrollViewSize.width / newZoomScale
+      let h = scrollViewSize.height / newZoomScale
+      let x = pointInView.x - (w / 2.0)
+      let y = pointInView.y - (h / 2.0)
+
+    let rectToZoomTo = CGRect(x: x, y: y, width: w, height: h)
+ 
+        scrollView.zoom(to: rectToZoomTo, animated: true)
+    }
+         
+    // MARK: - ScrollView zoom, drag etc
+ 
+    func updateMinZoomScaleForSize(_ size: CGSize) {
+      let widthScale = size.width / imageView.bounds.width
+      let heightScale = size.height / imageView.bounds.height
+      let minScale = min(widthScale, heightScale)
+        
+      scrollView.minimumZoomScale = minScale
+      scrollView.zoomScale = minScale
+        
+    }
+    func updateConstraintsForSize(_ size: CGSize) {
+        
+        let yOffset = max(0, (size.height - imageView.frame.height) / 2)
+        imageViewTopConstraint.constant = yOffset
+        imageViewBottomConstraint.constant = yOffset
+        
+        let xOffset = max(0, (size.width - imageView.frame.width) / 2)
+        imageViewLeadingConstraint.constant = xOffset
+        imageViewTrailingConstraint.constant = xOffset
+        
+        view.layoutIfNeeded()
+    }
+}
